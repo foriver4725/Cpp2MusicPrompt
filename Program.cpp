0036182e575eed15a4bc6bee26d078a9ac3d77ab
foriver4725 <hrt67@icloud.com>
2026-05-06 22:00:06 +0900

    refactor: restructure main program and add AST event tracking

--- Program.cpp
+++ Program.cpp
@@ -1,16 +1,34 @@
-int main()
-{
+int Add(int a, int b) {
+    int result = a + b;
+    return result;
+}
+
+int main() {
     int x = 10;
+    int y = 0;
 
-    if (x > 0)
-    {
-        x++;
+    if (x > 0) {
+        y = Add(x, 5);
+    } else {
+        y = -1;
     }
 
-    if (x > 5)
-    {
-        x--;
+    for (int i = 0; i < 3; i++) {
+        y += i;
     }
 
-    return 0;
+    while (y < 20) {
+        y++;
+    }
+
+    do {
+        y--;
+    } while (y > 10);
+
+    {
+        int scopedValue = y * 2;
+        y = scopedValue;
+    }
+
+    return y;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include "clang/AST/AST.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
@@ -11,71 +15,251 @@
 using namespace clang;
 using namespace clang::tooling;
 
-class IfCounterVisitor : public RecursiveASTVisitor<IfCounterVisitor>
-{
+struct AstEvent {
+    unsigned offset;
+    unsigned line;
+    unsigned column;
+    int priority;
+
+    std::string kind;
+    std::string name;
+};
+
+class AstEventVisitor : public RecursiveASTVisitor<AstEventVisitor> {
 public:
-    bool VisitIfStmt(IfStmt* stmt)
-    {
-        ++count;
+    explicit AstEventVisitor(ASTContext &context)
+        : context(context),
+          sourceManager(context.getSourceManager()) {
+    }
+
+    bool VisitFunctionDecl(FunctionDecl *decl) {
+        if (!IsInMainFile(decl))
+            return true;
+
+        if (!decl->isThisDeclarationADefinition())
+            return true;
+
+        AddEvent(
+            decl->getBeginLoc(),
+            0,
+            "FunctionDefinition",
+            decl->getNameAsString());
+
         return true;
     }
 
-    int GetCount() const
-    {
-        return count;
+    bool VisitVarDecl(VarDecl *decl) {
+        if (!IsInMainFile(decl))
+            return true;
+
+        // 関数引数は除外
+        if (isa<ParmVarDecl>(decl))
+            return true;
+
+        AddEvent(
+            decl->getBeginLoc(),
+            0,
+            "VariableDefinition",
+            decl->getNameAsString());
+
+        return true;
+    }
+
+    bool VisitIfStmt(IfStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getBeginLoc(), 0, "IfStatement", "");
+        return true;
+    }
+
+    bool VisitForStmt(ForStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getBeginLoc(), 0, "ForLoop", "");
+        return true;
+    }
+
+    bool VisitWhileStmt(WhileStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getBeginLoc(), 0, "WhileLoop", "");
+        return true;
+    }
+
+    bool VisitDoStmt(DoStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getBeginLoc(), 0, "DoWhileLoop", "");
+        return true;
+    }
+
+    bool VisitCompoundStmt(CompoundStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getLBracLoc(), 0, "ScopeBlockStart", "{}");
+        AddEvent(stmt->getRBracLoc(), 10, "ScopeBlockEnd", "{}");
+
+        return true;
+    }
+
+    bool VisitReturnStmt(ReturnStmt *stmt) {
+        if (!IsInMainFile(stmt))
+            return true;
+
+        AddEvent(stmt->getBeginLoc(), 0, "ReturnStatement", "");
+        return true;
+    }
+
+    bool VisitCallExpr(CallExpr *expr) {
+        if (!IsInMainFile(expr))
+            return true;
+
+        const FunctionDecl *callee = expr->getDirectCallee();
+
+        AddEvent(
+            expr->getBeginLoc(),
+            0,
+            "FunctionCall",
+            callee ? callee->getNameAsString() : "");
+
+        return true;
+    }
+
+    void PrintEvents() {
+        std::sort(
+            events.begin(),
+            events.end(),
+            [](const AstEvent &a, const AstEvent &b) {
+                if (a.offset != b.offset)
+                    return a.offset < b.offset;
+
+                return a.priority < b.priority;
+            });
+
+        for (const AstEvent &event: events) {
+            std::cout
+                    << "[" << event.line
+                    << ":" << event.column
+                    << "] "
+                    << event.kind;
+
+            if (!event.name.empty()) {
+                std::cout << " : " << event.name;
+            }
+
+            std::cout << std::endl;
+        }
     }
 
 private:
-    int count = 0;
+    ASTContext &context;
+    SourceManager &sourceManager;
+
+    std::vector<AstEvent> events;
+
+    bool IsInMainFile(const Decl *decl) const {
+        return IsInMainFile(decl->getBeginLoc());
+    }
+
+    bool IsInMainFile(const Stmt *stmt) const {
+        return IsInMainFile(stmt->getBeginLoc());
+    }
+
+    bool IsInMainFile(SourceLocation loc) const {
+        loc = sourceManager.getSpellingLoc(loc);
+
+        return
+                loc.isValid() &&
+                sourceManager.isWrittenInMainFile(loc);
+    }
+
+    void AddEvent(
+        SourceLocation loc,
+        int priority,
+        std::string kind,
+        std::string name) {
+        loc = sourceManager.getSpellingLoc(loc);
+
+        if (!loc.isValid())
+            return;
+
+        AstEvent event;
+
+        event.offset =
+                sourceManager.getFileOffset(loc);
+
+        event.line =
+                sourceManager.getSpellingLineNumber(loc);
+
+        event.column =
+                sourceManager.getSpellingColumnNumber(loc);
+
+        event.priority = priority;
+
+        event.kind = std::move(kind);
+        event.name = std::move(name);
+
+        events.push_back(std::move(event));
+    }
 };
 
-class IfCounterConsumer : public ASTConsumer
-{
+class AstEventConsumer : public ASTConsumer {
 public:
-    void HandleTranslationUnit(ASTContext& context) override
-    {
-        visitor.TraverseDecl(context.getTranslationUnitDecl());
+    explicit AstEventConsumer(ASTContext &context)
+        : visitor(context) {
+    }
 
-        std::cout << "If count: "
-                  << visitor.GetCount()
-                  << std::endl;
+    void HandleTranslationUnit(ASTContext &context) override {
+        visitor.TraverseDecl(
+            context.getTranslationUnitDecl());
+
+        visitor.PrintEvents();
     }
 
 private:
-    IfCounterVisitor visitor;
+    AstEventVisitor visitor;
 };
 
-class IfCounterAction : public ASTFrontendAction
-{
+class AstEventAction : public ASTFrontendAction {
 public:
     std::unique_ptr<ASTConsumer> CreateASTConsumer(
-        CompilerInstance& compiler,
-        llvm::StringRef file) override
-    {
-        return std::make_unique<IfCounterConsumer>();
+        CompilerInstance &compiler,
+        llvm::StringRef file) override {
+        return std::make_unique<AstEventConsumer>(
+            compiler.getASTContext());
     }
 };
 
-static llvm::cl::OptionCategory ToolCategory("if-counter");
+static llvm::cl::OptionCategory ToolCategory(
+    "cpp2-music-prompt");
 
-int main(int argc, const char** argv)
-{
+int main(int argc, const char **argv) {
     auto expectedParser =
-        CommonOptionsParser::create(argc, argv, ToolCategory);
+            CommonOptionsParser::create(
+                argc,
+                argv,
+                ToolCategory);
 
-    if (!expectedParser)
-    {
-        llvm::errs() << expectedParser.takeError();
+    if (!expectedParser) {
+        llvm::errs()
+                << expectedParser.takeError();
+
         return 1;
     }
 
-    CommonOptionsParser& optionsParser =
-        expectedParser.get();
+    CommonOptionsParser &optionsParser =
+            expectedParser.get();
 
     ClangTool tool(
         optionsParser.getCompilations(),
         optionsParser.getSourcePathList());
 
     return tool.run(
-        newFrontendActionFactory<IfCounterAction>().get());
+        newFrontendActionFactory<AstEventAction>()
+        .get());
 }
